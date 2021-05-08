89e23b6ea49777ee2d21b6e1339392394e20f5cf
Gustavo Pereira <ghapereira@gmail.com>
2021-05-08 09:04:32 -0300

    Add redis and volume to project

--- chat/app/main.py
+++ chat/app/main.py
@@ -1,13 +1,44 @@
 from typing import Optional
 
 from fastapi import FastAPI
+from pydantic import BaseModel
+import redis
+
+
+class LoginInfo(BaseModel):
+    username: str
+    password: str
+
 
 app = FastAPI()
 
+cache = redis.Redis(host='redis', port=6379)
 
 @app.get('/')
 def read_root() -> dict:
+    with open('/var/data/db.txt', 'w+') as fp:
+        fp.write('Data written!')
     return {'Hello': 'World'}
+
+
+@app.post('/login')
+def login(login_info: LoginInfo) -> dict:
+    # verify login
+    # if not logged in, log itself
+    # if logged, refresh token
+    count = get_hit_count()
+    return f'Hello world! I have been seen {count} times.\n'
+
+def get_hit_count() -> int:
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
 
 
 @app.get('/items/{item_id}')
