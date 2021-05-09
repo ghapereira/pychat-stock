cb8fd2075ac27844f32037dc094fa24bcb70aab4
Gustavo Pereira <ghapereira@gmail.com>
2021-05-09 07:43:55 -0300

    Start using RabbitMQ, change bot container

--- bot/app/main.py
+++ bot/app/main.py
@@ -1,17 +1,8 @@
-from typing import Optional
-
-from fastapi import FastAPI
-import requests
-
-app = FastAPI()
-
-
-@app.get('/')
-def read_root() -> dict:
-    r = requests.get('http://chat')
-    return {'From bot': r.text}
-
-
-@app.get('/items/{item_id}')
-def read_item(item_id: int, q: Optional[str] = None) -> dict:
-    return {'item_id': item_id, 'q': q}
+print('\n\n\n\t\tHELLO IM THE BOT!!!!!\n\n\n')
+print('\n\n\n\t\tHELLO IM THE BOT!!!!!\n\n\n')
+print('\n\n\n\t\tHELLO IM THE BOT!!!!!\n\n\n')
+print('\n\n\n\t\tHELLO IM THE BOT!!!!!\n\n\n')
+print('\n\n\n\t\tHELLO IM THE BOT!!!!!\n\n\n')
+print('\n\n\n\t\tHELLO IM THE BOT!!!!!\n\n\n')
+print('\n\n\n\t\tHELLO IM THE BOT!!!!!\n\n\n')
+print('\n\n\n\t\tHELLO IM THE BOT!!!!!\n\n\n')
